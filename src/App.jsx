1a8acc4862a0c31445ebd93a8f8d4dcaf38e0933
akbarsazish <sazishaliakbar@gmail.com>
2024-01-21 21:00:35 +0330

    create new the main branch

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter} from "react-router-dom";
+
+import {About, Contact, Tech, Works, Experience, Feedbacks, Navbar, Hero, StarsCanvas} from "./components";
+
+const App = () => {
+  return (
+    <BrowserRouter >
+      <div className="relateive z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center"> 
+          <Navbar />
+          <Hero />
+         </div>
+
+         <About />
+         <Experience />
+         <Tech />
+         <Works />
+         <Feedbacks />
+         
+      </div>
+      <div className="relative z-0">
+        <Contact />
+        <StarsCanvas />
+      </div>
+  </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/components/StarsCanvas.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function StarsConvas() {
+  return (
+    <div>StarCanvas</div>
+  )
+}
+
+export default StarsConvas
